b112f2cd97a831dbfddab7fe51fc17a1b940f3fd
shiju kumar <r.shijukumar@gmail.com>
2022-08-02 12:06:59 +0530

    initial commit

--- sumOfn.py
+++ sumOfn.py
@@ -6,21 +6,4 @@ for num in range (n+1):
     total = total + num 
 print(total)
 
-#sum of first n numbers using while
-total = 0
-n = int(input('enter the nth number : '))
-while  n >=0:
-    total = total + n 
-    n -= 1
-print(total)
 
-#sum of first n numbers using function
-
-def sumofN(n):
-    total = 0
-    for num in range (n+1):
-        total = total + num
-    print(total)
-
-#calling the function sumofN
-sumofN(5)
