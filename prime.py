d05a7c0364c5239ec2ed5e4cb7d9efd6b30b168a
shiju kumar <r.shijukumar@gmail.com>
2022-08-03 12:31:55 +0530

    prime and factors

--- prime.py
+++ prime.py
@@ -8,9 +8,25 @@ factors = []
 for i in range(1, n+1):
     rem = n%i
     if rem == 0:
-        factors.append(rem)
+        factors.append(i)
 if len(factors) == 2:
     print(f"{n} is prime")
 elif len(factors) > 2:
     print(f"{n} is composite")
+
     
+#program to check if a number is prime and to find the factors
+
+n = int(input('Enter the number: '))
+#1 is neither prime nor composite
+if n == 1:
+    print( "1 is Neither prime nor composite")
+factors = []
+for i in range(1, n+1):
+    rem = n%i
+    if rem == 0:
+        factors.append(i)
+if len(factors) == 2:
+    print(f"{n} is prime\n The factors are {factors}")
+elif len(factors) > 2:
+    print(f"{n} is composite\n The factors are {factors}")
