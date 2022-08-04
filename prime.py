85d36be9a3751661cbec4ca5fc75c76cb868e0d7
shiju kumar <r.shijukumar@gmail.com>
2022-08-04 09:45:16 +0530

    using functions

--- prime.py
+++ prime.py
@@ -31,6 +31,7 @@ if len(factors) == 2:
 elif len(factors) > 2:
     print(f"{n} is composite\n The factors are {factors}")
 
+
 #To print the prime numbers between given values
 start = int(input('Enter the starting number: '))
 stop = int(input("Enter the ending number: "))
@@ -46,3 +47,26 @@ for n in range (start, stop):
         prime.append(n)
                 
 print(prime)
+
+
+#Check for prime numbers and list the factors using function
+
+def is_prime(n):
+    if n == 1:
+        print( "1 is Neither prime nor composite")
+    factors = []
+    for i in range(1, n+1):
+        rem = n%i
+        if rem == 0:
+            factors.append(i)
+    if len(factors) == 2:
+        print(f"{n} is prime\n The factors are {factors}")
+    elif len(factors) > 2:
+        print(f"{n} is composite\n The factors are {factors}")
+
+#calling the function
+
+is_prime(7)
+
+for x in range(41240,41260):
+    is_prime(x)
