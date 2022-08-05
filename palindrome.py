4561a524ca97441522dcffebd595826f2bea610e
shiju kumar <r.shijukumar@gmail.com>
2022-08-05 22:17:47 +0530

    palindrome

---
+++ palindrome.py
@@ -0,0 +1,9 @@
+#program to check palindrome
+
+s = input("Please enter a string: ")
+z = s[::-1]
+print(z)
+if s == z:
+    print("The string is a palindrome")
+else:
+    print("The string is not a palindrome")
